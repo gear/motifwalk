5b862a8401e9deb34acc3ac21fc452b54a61a9b7
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-07-07 14:52:27 +0900

    create app version

---
+++ research/src/word2vec_tensorflow/word2vec.py
@@ -0,0 +1,12 @@
+"""Multi-threaded word2vec mini-batched skip-gram model.
+"""
+# Origin: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/embedding/word2vec.py
+# Coding: utf-8
+# Filename: word2vec.py - Python 2.7
+# Created: 2016-07-07 v0.0
+# Description: This model is proposed in ICLR 2013. arvix:1301.3781
+## v0.0: File copied from origin for learning purpose.
+
+from __future__ import
+
+
