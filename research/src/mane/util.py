9b884a6c08f454cb1e902e91647c8f923cb88c11
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-07-20 16:48:15 +0900

    pickle amazon graph, better txt reading

--- research/src/mane/util.py
+++ research/src/mane/util.py
@@ -27,25 +27,30 @@ def txt_edgelist_to_pickle(filename, pickle_name='default.graph'):
 
   """
   assert os.path.exists(filename), 'File not found. Please check: %s' % (filename)
-  datatext = open(filename, 'rb')
-  graph = dict()
-  for textline in datatext.readlines():
-    edge = textline.split(' ')
-    assert len(edge) == 2, 'Invalid data line.'
-    try:
-      t_edge = (int(edge[0]), int(edge[1]))
-    except Exception:
-      t_edge = (edge[0], edge[1])
-    if graph.has_key(t_edge[0]):
-      graph[t_edge[0]].append(t_edge[1])
-    else:
-      graph[t_edge[0]] = [t_edge[1]]
-    if graph.has_key(t_edge[1]):
-      graph[t_edge[1]].append(t_edge[0])
-    else:
-      graph[t_edge[1]] = [t_edge[0]] 
+  # Read lines and create graph dict
+  with open(filename, 'rb') as datatext:
+    graph = dict()
+    for textline in datatext.readlines():
+      # Skip comments
+      if textline[0] == '#': 
+        continue
+      edge = textline.split() # no arg -> white spaces
+      assert len(edge) == 2, 'Invalid data line: %s' % textline
+      try:
+        t_edge = (int(edge[0]), int(edge[1]))
+      except Exception:
+        t_edge = (edge[0], edge[1])
+      if graph.has_key(t_edge[0]):
+        graph[t_edge[0]].append(t_edge[1])
+      else:
+        graph[t_edge[0]] = [t_edge[1]]
+      if graph.has_key(t_edge[1]):
+        graph[t_edge[1]].append(t_edge[0])
+      else:
+        graph[t_edge[1]] = [t_edge[0]] 
   print('Created graph with %d nodes' % len(graph))
   print('Pickling to file: %s' % pickle_name)
-  pfile = open(pickle_name, 'wb')
-  pickle.dump(graph, pfile, pickle.HIGHEST_PROTOCOL)
+  # Save as a pickle file
+  with open(pickle_name, 'wb') as pfile:
+    pickle.dump(graph, pfile, pickle.HIGHEST_PROTOCOL)
   return graph
