ed9bbdc3901dfa6a95f7898454711bf21e1ba609
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-03 04:16:25 +0900

    clean bad models

--- research/src/mane/test.py
+++ research/src/mane/test.py
@@ -24,8 +24,8 @@ import matplotlib.pyplot as plt
 
 fb = g.graph_from_pickle('data/egonets.graph')
 
-name_rand = 'nce_egonets_e1500000_ne2_ns2_nw10_wl10_ws2_spe1_adam_rand'
-name_motif = 'nce_egonets_e1500000_ne2_ns2_nw10_wl10_ws2_spe1_adam_motif'
+name_rand = 'nce_egonets_e2_ne15_ns2_nw5_wl10_ws3_spe3433150_adam_rand'
+name_motif = 'nce_egonets_e2_ne15_ns2_nw5_wl10_ws3_spe3433150_adam_motif'
 
 rand_train = True
 motif_train = True
@@ -33,22 +33,22 @@ motif_train = True
 if not rand_train:
   pass
 else:
-  model_r = e.EmbeddingNet(graph=fb, epoch=1500000, neg_samp=2, 
-                           num_skip=2, num_walk=10, walk_length=10, 
-                           window_size=2, samples_per_epoch=1)
+  model_r = e.EmbeddingNet(graph=fb, epoch=2, neg_samp=2, 
+                           num_skip=2, num_walk=5, walk_length=10, 
+                           window_size=3, samples_per_epoch=3433150)
   adam_opt = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-  model_r.build(optimizer='rmsprop')
-  model_r.train(mode='random_walk', verbose=0)
+  model_r.build(optimizer='adam')
+  model_r.train(mode='random_walk')
   weight_r = model_r._model.get_weights()
 if not motif_train:
   pass
 else:
-  model_m = e.EmbeddingNet(graph=fb, epoch=1500000, neg_samp=2, 
-                           num_skip=2, num_walk=10, walk_length=10, 
-                           window_size=2, samples_per_epoch=1)
+  model_m = e.EmbeddingNet(graph=fb, epoch=2, neg_samp=2, 
+                           num_skip=2, num_walk=5, walk_length=10, 
+                           window_size=3, samples_per_epoch=3433150)
   adam_opt = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-  model_m.build(optimizer='sgd')
-  model_m.train(mode='motif_walk', threads=8)
+  model_m.build(optimizer='adam')
+  model_m.train(mode='motif_walk')
   weight_m = model_m._model.get_weights()
 
 # Save model
