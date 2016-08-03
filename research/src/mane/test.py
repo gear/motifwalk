e5256ffd2d2564d98793954f2668736d8d0c2472
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-03 18:55:47 +0900

    test with karate graph

--- research/src/mane/test.py
+++ research/src/mane/test.py
@@ -23,26 +23,27 @@ from sklearn.manifold import TSNE
 from sklearn.preprocessing import normalize
 import matplotlib.pyplot as plt
 
-fb = g.graph_from_pickle('data/egonets.graph')
+fb = g.graph_from_pickle('data/karate.graph')
 
-name_rand = 'nce_egonets_e10_ne15_ns2_nw5_wl10_ws3_it1_adam_rand'
-name_motif = 'nce_egonets_e10_ne15_ns2_nw5_wl10_ws3_it1_adam_motif'
-name_contrast = 'nce_egonets_e10_ne15_ns2_nw5_wl20_ws3_it1_adam_contrast'
+name_rand = 'nce_karate_e10_ed5_ne5_ns1_nw5_wl5_ws2_it1_adam_rand'
+name_motif = 'nce_karate_e10_ed5_ne5_ns1_nw5_wl5_ws2_it1_adam_motif'
+name_contrast = 'nce_karate_e10_ed5_ne5_ns1_nw5_wl5_ws2_it1_adam_contrast'
 
-rand_train = False
-motif_train = False
+rand_train = True
+motif_train = True
 contrast_train = True
 
-save_rand = False
-save_motif = False
+save_rand = True
+save_motif = True
 save_contrast = True
 
 if not rand_train:
   pass
 else:
-  model_r = e.EmbeddingNet(graph=fb, epoch=10, emb_dim=200, neg_samp=15, 
-                           num_skip=2, num_walk=5, walk_length=10, 
-                           window_size=3, iters=1.0)
+  print('training la')
+  model_r = e.EmbeddingNet(graph=fb, epoch=10, emb_dim=5, neg_samp=5, 
+                           num_skip=1, num_walk=5, walk_length=5, 
+                           window_size=2, iters=5.0)
   adam_opt = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
   model_r.build(optimizer='adam')
   model_r.train(mode='random_walk')
@@ -51,9 +52,9 @@ else:
 if not motif_train:
   pass
 else:
-  model_m = e.EmbeddingNet(graph=fb, epoch=10, emb_dim=200, neg_samp=15,
-                           num_skip=2, num_walk=5, walk_length=10, 
-                           window_size=3, iters=1.0)
+  model_m = e.EmbeddingNet(graph=fb, epoch=10, emb_dim=5, neg_samp=5,
+                           num_skip=1, num_walk=5, walk_length=5, 
+                           window_size=2, iters=5.0)
   adam_opt = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
   model_m.build(optimizer='adam')
   model_m.train(mode='motif_walk')
@@ -62,9 +63,9 @@ else:
 if not contrast_train:
   pass
 else:
-  model_c = e.EmbeddingNet(graph=fb, epoch=10, emb_dim=200, neg_samp=15,
-                           num_skip=2, num_walk=5, walk_length=20,
-                           window_size=3, iters=1.0) # reset default at 0.3
+  model_c = e.EmbeddingNet(graph=fb, epoch=10, emb_dim=5, neg_samp=5,
+                           num_skip=1, num_walk=5, walk_length=5,
+                           window_size=2, iters=5.0) # reset default at 0.3
   model_c.build(optimizer='adam')
   model_c.train_mce()
   weight_c = model_c._model.get_weights()
@@ -87,57 +88,27 @@ weight_r_avg = normalize(weight_r[0])
 weight_m_avg = normalize(weight_m[0])
 weight_c_avg = normalize(weight_c[0])
 
-# Save or load tsne
-if not os.path.exists(name_rand+'.tsne') and save_rand:
-  tsne_weight_r_in = TSNE(learning_rate=100).fit_transform(weight_r[0])
-  tsne_weight_r_out = TSNE(learning_rate=100).fit_transform(weight_r[1])
-  with open(name_rand+'.tsne', 'wb') as f:
-    tsne = (tsne_weight_r_in, tsne_weight_r_out)
-    p.dump(tsne, f, p.HIGHEST_PROTOCOL)
+tsne_weight_r_norm = TSNE(learning_rate=100).fit_transform(weight_r_avg)
+tsne_weight_m_norm = TSNE(learning_rate=100).fit_transform(weight_m_avg)
+tsne_weight_c_norm = TSNE(learning_rate=100).fit_transform(weight_c_avg)
 
-if not os.path.exists(name_motif+'.tsne') and save_motif:
-  tsne_weight_m_in = TSNE(learning_rate=100).fit_transform(weight_m[0])
-  tsne_weight_m_out = TSNE(learning_rate=100).fit_transform(weight_m[1])
-  with open(name_motif+'.tsne', 'wb') as f:
-    tsne = (tsne_weight_m_in, tsne_weight_m_out)
-    p.dump(tsne, f, p.HIGHEST_PROTOCOL)
-
-if not os.path.exists(name_contrast+'_avg.tsne') and save_contrast:
-  tsne_weight_c = TSNE(learning_rate=100).fit_transform(weight_c[0])
-  tsne_weight_c_norm = TSNE(learning_rate=100).fit_transform(weight_c_avg)
-  with open(name_contrast+'_norm.tsne', 'wb') as f:
-    p.dump(tsne_weight_c, f, p.HIGHEST_PROTOCOL)
-
-#fig = plt.figure(figsize=(10,15))
-#fig.suptitle(name_rand[:-5], fontsize=16)
-#a=plt.subplot(321)
-#a.set_title("Random walk in embedding")
-#a.scatter(tsne_weight_r_in[:,0], tsne_weight_r_in[:,1])
-#b=plt.subplot(322)
-#b.set_title("Random walk out embedding")
-#b.scatter(tsne_weight_r_out[:,0], tsne_weight_r_out[:,1])
-#c=plt.subplot(323)
-#c.set_title("Motif walk in embedding")
-#c.scatter(tsne_weight_m_in[:,0], tsne_weight_m_in[:,1])
-#d=plt.subplot(324)
-#d.set_title("Motif walk out embedding")
-#d.scatter(tsne_weight_m_out[:,0], tsne_weight_m_out[:,1])
-#e=plt.subplot(325)
-#e.set_title("Random walk average embedding")
-#e.scatter(tsne_weight_r[:,0], tsne_weight_r[:,1])
-#f=plt.subplot(326)
-#f.set_title("Motif walk average embedding")
-#f.scatter(tsne_weight_m[:,0], tsne_weight_m[:,1])
-#plt.savefig(name_rand[:-5]+'.png')
-#plt.show()
-
-fig = plt.figure(figsize=(5,10))
-fig.suptitle(name_contrast, fontsize=16)
-a = plt.subplot(121)
-a.set_title("Un-normalized embedding")
-a.scatter(tsne_weight_c[:,0], tsne_weight_c[:,1])
-b = plt.subplot(122)
-b.set_title("Normalized embedding")
-b.scatter(tsne_weight_c_norm[:,0], tsne_weight_c_norm[:,1])
-plt.savefig(name_contrast+'.png')
+fig = plt.figure(figsize=(15,45))
+fig.suptitle(name_rand[:-5], fontsize=16)
+a=plt.subplot(311)
+a.set_title("Random walk embedding")
+a.scatter(tsne_weight_r_norm[:,0], tsne_weight_r_norm[:,1])
+for i, xy in enumerate(tsne_weight_r_norm):
+  a.annotate('%s' % i, xy=xy, textcoords='data')
+b=plt.subplot(312)
+b.set_title("Motif walk embedding")
+b.scatter(tsne_weight_m_norm[:,0], tsne_weight_m_norm[:,1])
+for i, xy in enumerate(tsne_weight_m_norm):
+  b.annotate('%s' % i, xy=xy, textcoords='data')
+c=plt.subplot(313)
+c.set_title("Contrast walk embedding")
+c.scatter(tsne_weight_c_norm[:,0], tsne_weight_c_norm[:,1])
+for i, xy in enumerate(tsne_weight_c_norm):
+  c.annotate('%s' % i, xy=xy, textcoords='data')
+plt.savefig(name_rand[:-5]+'.png')
 plt.show()
+
