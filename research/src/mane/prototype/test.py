b62443a50477e5b84bef807d4048976cd11b1629
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-10 01:26:32 +0900

    add node2vec for compare - notworking

--- research/src/mane/prototype/test.py
+++ research/src/mane/prototype/test.py
@@ -3,9 +3,9 @@ import embeddings as e
 import pickle as p
 
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
-model=e.EmbeddingNet(graph=bc)
-model.build()
+model=e.EmbeddingNet(graph=bc, neg_samp=10)
+model.build(optimizer='sgd')
 model.train(batch_size=400)
 weights = model.get_weights()
-with open('default_random_bs400.weights', 'wb') as f:
+with open('random_bs400_ns10.weights', 'wb') as f:
     p.dump(weights, f, p.HIGHEST_PROTOCOL)
