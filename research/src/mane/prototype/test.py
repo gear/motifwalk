20b609521662631211dd125362316a60ba219b11
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-10 09:37:22 +0900

    sync result

--- research/src/mane/prototype/test.py
+++ research/src/mane/prototype/test.py
@@ -5,7 +5,7 @@ import pickle as p
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
 model=e.EmbeddingNet(graph=bc, num_walk=10, neg_samp=15)
 model.build()
-model.train(num_nodes_per_batch=200, batch_size=256)
+model.train(num_nodes_per_batch=200, batch_size=512)
 weights = model.get_weights()
-with open('random_npb200_bs256_nw10_ns15.weights', 'wb') as f:
+with open('random_npb200_bs512_nw10_ns15.weights', 'wb') as f:
     p.dump(weights, f, p.HIGHEST_PROTOCOL)
