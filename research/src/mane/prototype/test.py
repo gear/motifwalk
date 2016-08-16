50233ac8e7112b845eb5b6cea0ac3adc5dbe53a5
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-17 01:08:36 +0900

    sync results

--- research/src/mane/prototype/test.py
+++ research/src/mane/prototype/test.py
@@ -2,10 +2,10 @@ import graph as g
 import embeddings as e
 import pickle as p
 
-bc = g.graph_from_pickle('../data/blogcatalog3.graph')
-model=e.EmbeddingNet(graph=bc, num_walk=10, neg_samp=15)
+bc = g.graph_from_pickle('../data/youtube.graph')
+model=e.EmbeddingNet(graph=bc, num_walk=1, neg_samp=10, emb_dim=8)
 model.build()
-model.train(num_nodes_per_batch=200, batch_size=1024)
+model.train(mode='motif_walk', num_nodes_per_batch=400, batch_size=1024)
 weights = model.get_weights()
-with open('random_npb200_bs1024_nw10_ns15.weights', 'wb') as f:
+with open('youtube_motif_npb400_bs1024_nw10_ns10.weights', 'wb') as f:
     p.dump(weights, f, p.HIGHEST_PROTOCOL)
