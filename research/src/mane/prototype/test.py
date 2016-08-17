88f7c5e9ad28a3532db1977b5eeba51e4c1f96a2
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-17 18:23:17 +0900

    quick run

--- research/src/mane/prototype/test.py
+++ research/src/mane/prototype/test.py
@@ -3,9 +3,9 @@ import embeddings as e
 import pickle as p
 
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
-model=e.EmbeddingNet(graph=bc, num_walk=20, neg_samp=15, walk_length=80)
+model=e.EmbeddingNet(graph=bc, num_walk=1, neg_samp=15, walk_length=20, window_size=6)
 model.build()
 model.train(mode='motif_walk',num_nodes_per_batch=100, batch_size=128)
 weights = model.get_weights()
-with open('BC3012.weights', 'wb') as f:
+with open('BC3014.weights', 'wb') as f:
     p.dump(weights, f, p.HIGHEST_PROTOCOL)
