a0e5e4d9df4e518ef08cff0a82978dd1a47da119
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-17 11:08:15 +0900

    0.6 bias motif walk

--- research/src/mane/prototype/test.py
+++ research/src/mane/prototype/test.py
@@ -3,9 +3,9 @@ import embeddings as e
 import pickle as p
 
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
-model=e.EmbeddingNet(graph=bc, num_walk=5, neg_samp=10, walk_length=80)
+model=e.EmbeddingNet(graph=bc, num_walk=10, neg_samp=15, walk_length=60)
 model.build()
-model.train(mode='motif_walk',num_nodes_per_batch=400, batch_size=512)
+model.train(mode='motif_walk',num_nodes_per_batch=400, batch_size=1024)
 weights = model.get_weights()
-with open('BC3006.weights', 'wb') as f:
+with open('BC3009.weights', 'wb') as f:
     p.dump(weights, f, p.HIGHEST_PROTOCOL)
