aef7e1f2e8c3f9536080789187219466b7068f1e
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-18 11:29:18 +0900

    large batch size

--- research/src/mane/prototype/test_gtx.py
+++ research/src/mane/prototype/test_gtx.py
@@ -5,7 +5,7 @@ import pickle as p
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
 model=e.EmbeddingNet(graph=bc, num_walk=10, neg_samp=15, walk_length=80, window_size=10)
 model.build()
-model.train(mode='motif_walk',num_nodes_per_batch=200, batch_size=1024)
+model.train(mode='motif_walk',num_nodes_per_batch=400, batch_size=8000)
 weights = model.get_weights()
-with open('BC3015.weights', 'wb') as f:
+with open('BC3017.weights', 'wb') as f:
     p.dump(weights, f, p.HIGHEST_PROTOCOL)
