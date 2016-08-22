e378e6513ca951d31aa7cd0598e26e4f679eaa67
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-22 21:02:28 +0900

    ex30

--- research/src/mane/prototype/test_gtx.py
+++ research/src/mane/prototype/test_gtx.py
@@ -5,7 +5,7 @@ import pickle as p
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
 embDim = 128
 
-posFunc = 'random_walk'
+posFunc = 'triangle_walk'
 posArgs = {'walk_length': 80, 'start_node': None, 'rand_seed': None,
             'reset': 0.0, 'walk_bias': 0.99, 'isNeg': False}
 negFunc = 'unigram'
@@ -18,7 +18,7 @@ numWalk = 10
 walkLength = 80
 windowSize = 10
 walkPerBatch = 400
-batchSize = 8000
+batchSize = 1000
 vb = 1
 
 model=e.EmbeddingNet(graph=bc, emb_dim=embDim)
@@ -29,5 +29,5 @@ model.train(pos_func=posFunc, neg_func=negFunc, epoch=ep,
             walk_per_batch=walkPerBatch, batch_size=batchSize, 
             verbose=vb)
 weights = model.get_weights()
-with open('embeddings/BC3029.weights', 'wb') as f:
+with open('embeddings/BC3030.weights', 'wb') as f:
   p.dump(weights, f, p.HIGHEST_PROTOCOL)
