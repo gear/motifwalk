bdc61e10d3608afe09c285b9449ae1207cd243fb
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-24 12:17:17 +0900

    ex43 adjust triangle walk

--- research/src/mane/prototype/test_gtx.py
+++ research/src/mane/prototype/test_gtx.py
@@ -5,14 +5,14 @@ import pickle as p
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
 embDim = 128
 
-posFunc = 'triangle_walk'
+posFunc = 'random_walk'
 posArgs = {'walk_length': 80, 'start_node': None, 'rand_seed': None,
             'reset': 0.0, 'walk_bias': 0.8, 'isNeg': False}
 negFunc = 'unigram'
 negArgs = {'walk_length': 80, 'start_node': None, 'rand_seed': None,
             'reset': 0.0, 'walk_bias': 0, 'isNeg': True}
 ep = 1
-negSamp = 30
+negSamp = 40
 numSkip = 5
 numWalk = 10
 walkLength = 80
@@ -37,6 +37,6 @@ model.train(pos_func=posFunc, neg_func=negFunc, epoch=ep,
 #            walk_per_batch=walkPerBatch, batch_size=batchSize, 
 #            verbose=vb)
 weights = model.get_weights()
-with open('embeddings/BC3042.weights', 'wb') as f:
+with open('embeddings/BC3043.weights', 'wb') as f:
   p.dump(weights, f, p.HIGHEST_PROTOCOL)
 
