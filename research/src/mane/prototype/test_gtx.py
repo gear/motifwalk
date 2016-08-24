a782535c705b8739f65113b8ffdb1fc1152e2a5b
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-24 10:07:06 +0900

    ex41 30 neg samps

--- research/src/mane/prototype/test_gtx.py
+++ research/src/mane/prototype/test_gtx.py
@@ -5,18 +5,18 @@ import pickle as p
 bc = g.graph_from_pickle('../data/blogcatalog3.graph')
 embDim = 128
 
-posFunc = 'random_walk'
+posFunc = 'triangle_walk'
 posArgs = {'walk_length': 80, 'start_node': None, 'rand_seed': None,
-            'reset': 0.0, 'walk_bias': 0.99, 'isNeg': False}
+            'reset': 0.0, 'walk_bias': 0.8, 'isNeg': False}
 negFunc = 'unigram'
 negArgs = {'walk_length': 80, 'start_node': None, 'rand_seed': None,
             'reset': 0.0, 'walk_bias': 0, 'isNeg': True}
 ep = 1
-negSamp = 25
+negSamp = 30
 numSkip = 5
 numWalk = 10
 walkLength = 80
-windowSize = 8
+windowSize = 15
 walkPerBatch = 400
 batchSize = 1000
 vb = 1
@@ -37,5 +37,6 @@ model.train(pos_func=posFunc, neg_func=negFunc, epoch=ep,
 #            walk_per_batch=walkPerBatch, batch_size=batchSize, 
 #            verbose=vb)
 weights = model.get_weights()
-with open('embeddings/BC3040.weights', 'wb') as f:
+with open('embeddings/BC3042.weights', 'wb') as f:
   p.dump(weights, f, p.HIGHEST_PROTOCOL)
+
