54d50998df0a1a1d62b200f758f69d5af700f2b9
gear <hoangnt@ai.cs.titech.ac.jp>
2017-01-03 16:12:25 +0900

    n2v constrain and triangle constrain

--- src/constrains.py
+++ src/constrains.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
-from numpy.random import choice
+from numpy.random import choice, rand
+from abc import ABCMeta, abstractmethod
 
 
 class Constrains(object):
@@ -8,9 +9,13 @@ class Constrains(object):
     """Contrains object to guide the random walk
     based on the current context."""
 
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
     def __init__(self):
         pass
 
+    @abstractmethod
     def select(self, **kwargs):
         pass
 
@@ -23,12 +28,38 @@ class R(Constrains):
         super().__init__()
         self._desc = """Random walk with no constrain."""
 
-    def select(self, curr_node, graph, data):
+    def select(self, curr_node, graph):
         
         """Select next random node in the graph
            based on the current nodes."""
 
         return choice(list(graph[curr_node]))
+
+class UTriangle(Constrains):
+
+    """Random walk in triangle manner."""
+
+    def __init__(self, enforce_prob=0.9):
+
+        """Create a triangle motif constrain object with the
+        probability of following the triangle pattern is
+        `enforce_prob`."""
+
+        super().__init__()
+        self._a = enforce_prob
+
+    def select(self, curr_node, graph, data):
+
+        """Select the next node in based on current
+        node and the current walk."""
+
+        triangle_nodes = [i for i in graph[curr_node]
+                       if set().union(graph[i], graph[curr_node])]
+        rand_num = rand()
+        if len(triangle_nodes) > 0:
+            if rand_num > self._a:
+                return choice(triangle_nodes)
+        return choice(graph.neighbors(curr_node))
 
 
 class N2V(Constrains):
@@ -36,50 +67,90 @@ class N2V(Constrains):
     """Node2Vec biased random walk."""
 
     def __init__(self, p=0.25, q=0.25):
-    
+
         """Create a constrain object as described
         in the Node2Vec paper."""
 
         super().__init__()
         self._desc = """Node2Vec walk with p and q."""
-        self._p = p 
+        self._p = p
         self._q = q
         self._alias_is_set = False
-        
-    def select(self, curr_node, graph, data):
+
+    def select(self, curr_node, nxgraph, data):
 
         """Select next random node in the graph
-           based on the defined hyperparameter
-           p and q."""     
+        based on the defined hyperparameter
+        p and q."""
 
-    def _alias_setup(self, probs):
 
-        """Compute utility lists for non-uniform sampling from
-        discrete distributions. Ref: hips.seas.harvard.edu"""
+    def _preprocess_transition_probs(self, nxgraph):
 
-        K = len(probs)
-        q = np.zeros(K)
-        J = np.zeros(K, dtype=np.int)
+        """Preprocessing of transition probs
+        for guiding the random walks."""
 
-        smaller = []
-        larger = []
-        for kk, prob in enumerate(probs):
-            q[kk] = K*prob
-            if q[kk] < 1.0:
-                smaller.append(kk)
-            else:
-                larger.append(kk)
+        alias_nodes = {}
+        for node in G.nodes():
+            unnormalized_probs = [nxgraph[node][nbr]['weight'] for
+                                  nbr in sorted(nxgraph.neighbors(node))]
+            norm_const = sum(unnormalized_probs)
+            normalized_probs = [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+            alias_nodes[node] = _alias_setup(normalized_probs)
 
-        while len(smaller) > 0 and len(larger) > 0:
-            small = smaller.pop()
-            large = larger.pop()
-            J[small] = large
-            q[large] = q[large] + q[small] - 1.0
-            if q[large] < 1.0:
-                smaller.append(large)
-            else:
-                larger.append(large)
+        alias_edges = {}
+        triads = {}
 
-        self._J = J
-        self._q = q
-        self._alias_is_set = True
+        if nxgraph.is_directed():
+            for edge in nxgraph.edges():
+                alias_edges[edge] = self._get_alias_edge(graph, edge[0], edge[1])
+        else:
+            pass
+
+def test():
+    print(nx)
+
+def _alias_setup(self, probs):
+
+    """Compute utility lists for non-uniform sampling from
+    discrete distributions. Ref: hips.seas.harvard.edu"""
+
+    k = len(probs)
+    q = np.zeros(k)
+    j = np.zeros(k, dtype=np.int)
+
+    smaller = []
+    larger = []
+    for kk, prob in enumerate(probs):
+        q[kk] = k*prob
+        if q[kk] < 1.0:
+            smaller.append(kk)
+        else:
+            larger.append(kk)
+
+    while len(smaller) > 0 and len(larger) > 0:
+        small = smaller.pop()
+        large = larger.pop()
+        j[small] = large
+        q[large] = q[large] + q[small] - 1.0
+        if q[large] < 1.0:
+            smaller.append(large)
+        else:
+            larger.append(large)
+
+    return j, q
+
+def _alias_draw(j, q):
+
+    """Draw sample from a non-uniform discrete distribution
+	using alias sampling."""
+
+    k = len(j)
+
+    kk = int(np.floor(np.random.rand()*k))
+    if np.random.rand() < q[kk]:
+        return kk
+    else:
+        return j[kk]
+
+if __name__ == '__main__':
+    test()
