18ca4ff390a0bc349a191959e5ad1da5fde667de
gear <hoangnt@ai.cs.titech.ac.jp>
2017-01-12 14:10:39 +0900

    run deepwalk and cora network analysis

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import pickle as p
-import networkx as nx
+import numpy as np
 
 dataloc = './../data/'
 
@@ -26,3 +26,26 @@ def load_blogcatalog():
         features = None
         labels = data['LILLabels']
     return graph, features, labels
+
+def load_embeddings(emb_file):
+    """Load graph embedding output from deepwalk, n2v to a numpy matrix."""
+    with open(emb_file, 'rb') as efile:
+        num_node, dim = map(int, efile.readline().split())
+        emb_matrix = np.ndarray(shape=(num_node, dim), dtype=np.float32)
+        for data in efile.readlines():
+            node_id, *vector = data.split()
+            node_id = int(node_id)
+            emb_matrix[node_id,:] = map(float, vector)
+    return emb_matrix
+
+"""https://github.com/gear/deepwalk/blob/master/example_graphs/scoring.py"""
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
+from itertools import izip
+from sklearn.metrics import f1_score
+from sklearn.utils import shuffle
+
+class TopKRanker(OneVsRestClassifier):
+    def predict(self, X, top_k_list):
+        assert X.shape[0] == len(top_k_list)
+        probs = np.
