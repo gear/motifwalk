4314fd2882e38ada65d2550ae0b85672a0c5a616
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-05-27 12:38:34 +0900

    remove line spaces

--- motifwalk/motifs/__init__.py
+++ motifwalk/motifs/__init__.py
@@ -17,6 +17,7 @@ class Motif:
         gt_graph.add_edge_list(edge_list)
         self.gt_motif = gt_graph
         self.nx_motif = nx_graph
+        self.size = nx_graph.size()
         self.anchors = anchors
         self.name = name
 
@@ -32,41 +33,29 @@ wedge = m3u_1
 
 m3_0 = Motif([(0,1), (1,0), (1,2), (2,1), (0,2), (2,0)],
              is_directed=True, name="m3_0")
-
 m3_1 = Motif([(0,1), (1,0), (2,1), (0,2), (2,0)],
              is_directed=True, name="m3_1")
-
 m3_2 = Motif([(0,1), (1,0), (2,0), (0,2)],
              is_directed=True, name="m3_2")
-
 m3_3 = Motif([(0,1), (1,0), (0,2), (1,2)],
              is_directed=True, name="m3_3")
-
 m3_4 = Motif([(0,1), (1,0), (2,0), (1,2)],
              is_directed=True, name="m3_4")
-
 m3_5 = Motif([(0,1), (1,0), (2,0), (2,1)],
              is_directed=True, name="m3_5")
-
 m3_6 = Motif([(0,1), (1,0), (2,0)],
              is_directed=True, name="m3_6")
-
 m3_7 = Motif([(0,1), (1,0), (0,2)],
              is_directed=True, name="m3_7")
-
 m3_8 = Motif([(0,1), (1,2), (2,0)],
              is_directed=True, name="m3_8")
-
 m3_9 = Motif([(0,1), (2,1), (2,0)],
               is_directed=True, name="m3_9")
 feed_forward = m3_9
-
 m3_10 = Motif([(0,1), (2,0)],
               is_directed=True, name="m3_10")
-
 m3_11 = Motif([(0,1), (0,2)],
               is_directed=True, name="m3_11")
-
 m3_12 = Motif([(1,0), (2,0)],
               is_directed=True, name="m3_12")
 
@@ -134,7 +123,7 @@ m4_38 = Motif([(0, 2), (1, 0), (2, 1), (3, 1), (3, 2)],
               is_directed=True, name='m4_38')
 m4_39 = Motif([(0, 3), (1, 2), (2, 0), (2, 1), (3, 1)],
               is_directed=True, name='m4_39')
-m4_40 = Motif([(0, 2), (1, 3), (2, 1), (2, 0), (3, 1)], 
+m4_40 = Motif([(0, 2), (1, 3), (2, 1), (2, 0), (3, 1)],
               is_directed=True, name='m4_40')
 m4_41 = Motif([(0, 2), (0, 1), (1, 3), (2, 3), (3, 0)],
               is_directed=True, name='m4_41')
