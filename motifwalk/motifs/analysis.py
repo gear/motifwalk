273543530847b0191b3e92bb33115f23050b6cf7
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-05-31 21:07:30 +0900

    fix bugs on motif isomorphism

--- motifwalk/motifs/analysis.py
+++ motifwalk/motifs/analysis.py
@@ -3,13 +3,14 @@ from itertools import combinations
 import numpy as np
 import networkx as nx
 from motifwalk.utils.Graph import GraphContainer
-from motifwalk.motifs import * # All pre-defined motifs
-import graph_tool as gt
-from graph_tool.all import * # Enable motif functions
+from motifwalk.motifs import all_u3, all_3, all_u4, all_4
+from graph_tool.all import Graph, motifs
 
 def count_motif(g, motif_object, rm=True):
     """Count the number of given motif and return the mapping list
-    to the vertices belongs to the motif.
+    to the vertices belongs to the motif. Note that `motifs` function
+    return a triplet, lossing pointer to the first element in This
+    triplet will make all PropertyMap orphan.
 
     Parameters:
     graph_container - GraphContainer - Graph storage object
@@ -26,7 +27,7 @@ def count_motif(g, motif_object, rm=True):
     rm, c, v_map = motifs(g, m.num_vertices(), motif_list=[m], return_maps=rm)
     return rm, c, v_map
 
-def construct_motif_graph(graph_container, motif):
+def construct_motif_graph(graph_container, motif, vertex_maps=None):
     """Construct and return a undirected gt graph containing
     motif relationship.
 
@@ -43,8 +44,9 @@ def construct_motif_graph(graph_container, motif):
     # graph_tool.Graph
     m_graph = Graph(directed=False)
     if vertex_maps is None:
-        _, _, vertex_maps = count_motif(graph, motif)
-    for prop in vertex_maps:
-        edges = [i for i in motif.anchored_edges(graph, prop.get_array())]
-        m_graph.add_edges_from(edges)
+        m, c, vertex_maps = count_motif(graph, motif)
+    for prop_list in vertex_maps:
+        for prop in prop_list:
+            edges = [i for i in motif.anchored_edges(graph, prop.get_array())]
+            m_graph.add_edge_list(edges)
     return m_graph
