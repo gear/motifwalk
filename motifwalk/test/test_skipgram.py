448fc989fecb1a5669f6d6adeff67dde8db1b0cc
gear <hoangnt@ai.cs.titech.acjp>
2017-06-02 13:38:53 +0900

    add gpu test

--- motifwalk/test/test_skipgram.py
+++ motifwalk/test/test_skipgram.py
@@ -1,7 +1,7 @@
 from motifwalk.utils import find_meta, set_dataloc, get_metadata
 from motifwalk.utils.Graph import GraphContainer
 from motifwalk.walks import undirected_randomwalk
-from motifwalk.models.skipgram import Skipgram
+from motifwalk.models.skipgram import Skipgram, ADAM
 from motifwalk.utils import timer
 import os
 import numpy as np
@@ -9,7 +9,7 @@ from sys import argv
 
 dataset_name = argv[1]
 tnum = argv[2]
-dloc = '/home/gear/Dropbox/CompletedProjects/motifwalk/data'
+dloc = '/home/hoangnt/motifwalk/data'
 set_dataloc(dloc)
 metadata = get_metadata()
 
@@ -18,6 +18,10 @@ def test(test_number):
         test1()
     elif '2' == test_number:
         test2()
+    elif '3' == test_number:
+        test3()
+    elif '4' == test_number:
+        test4()
     else:
         print("Test have not yet defined")
 
@@ -38,10 +42,74 @@ def test1():
     timer(False)
     print("Start training...")
     timer()
-    emb = model.train(walks, 10, 2, 2)
+    emb = model.train(data=walks, num_step=10, log_step=2, save_step=2)
     timer(False)
     print(type(emb))
     print(emb.shape)
+
+def test2():
+    network = GraphContainer(find_meta(dataset_name), dloc)
+    print("Generating gt graph...")
+    timer()
+    gt = network.get_gt_graph()
+    timer(False)
+    print("Creating Skipgram model...")
+    timer()
+    model = Skipgram(window_size=5, num_skip=2, num_nsamp=15)
+    model.build(num_vertices=gt.num_vertices(), learning_rate=0.001)
+    timer(False)
+    print("Generating random walk...")
+    timer()
+    walks, index = undirected_randomwalk(gt)
+    timer(False)
+    print("Start training...")
+    timer()
+    emb = model.train(data=walks, num_step=10000, log_step=2000, save_step=2)
+    timer(False)
+    np.save("cora.emb.npy", emb)
+
+def test3():
+    network = GraphContainer(find_meta(dataset_name), dloc)
+    print("Generating gt graph...")
+    timer()
+    gt = network.get_gt_graph()
+    timer(False)
+    print("Creating Skipgram model...")
+    timer()
+    model = Skipgram(window_size=5, num_skip=2, num_nsamp=15)
+    model.build(num_vertices=gt.num_vertices(), learning_rate=0.001, opt=ADAM)
+    timer(False)
+    print("Generating random walk...")
+    timer()
+    walks, index = undirected_randomwalk(gt)
+    timer(False)
+    print("Start training...")
+    timer()
+    emb = model.train(data=walks, num_step=1000000, log_step=2000, save_step=2)
+    timer(False)
+    np.save("cora.emb.npy", emb)
+
+def test4():
+    network = GraphContainer(find_meta(dataset_name), dloc)
+    print("Generating gt graph...")
+    timer()
+    gt = network.get_gt_graph()
+    timer(False)
+    print("Creating Skipgram model...")
+    timer()
+    model = Skipgram(window_size=5, num_skip=2, num_nsamp=15)
+    model.device = '/gpu:0'
+    model.build(num_vertices=gt.num_vertices(), learning_rate=0.001, opt=ADAM, batch_size=10000)
+    timer(False)
+    print("Generating random walk...")
+    timer()
+    walks, index = undirected_randomwalk(gt)
+    timer(False)
+    print("Start training...")
+    timer()
+    emb = model.train(data=walks, num_step=1000000, log_step=2000, save_step=2)
+    timer(False)
+    np.save("cora.emb.npy", emb)
 
 if __name__ == "__main__":
     test(tnum)
