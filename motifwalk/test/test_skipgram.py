85d582d99bb63397b371a00fd4a762883f793dea
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-06-15 20:51:22 +0900

    change data folder

--- motifwalk/test/test_skipgram.py
+++ motifwalk/test/test_skipgram.py
@@ -9,7 +9,7 @@ from sys import argv
 
 dataset_name = argv[1]
 tnum = argv[2]
-dloc = '/home/hoangnt/motifwalk/data'
+dloc = '/home/gear/Dropbox/CompletedProjects/motifwalk/data'
 set_dataloc(dloc)
 metadata = get_metadata()
 
@@ -20,8 +20,6 @@ def test(test_number):
         test2()
     elif '3' == test_number:
         test3()
-    elif '4' == test_number:
-        test4()
     else:
         print("Test have not yet defined")
 
@@ -78,28 +76,6 @@ def test3():
     timer()
     model = Skipgram(window_size=5, num_skip=2, num_nsamp=15)
     model.build(num_vertices=gt.num_vertices(), learning_rate=0.001, opt=ADAM)
-    timer(False)
-    print("Generating random walk...")
-    timer()
-    walks, index = undirected_randomwalk(gt)
-    timer(False)
-    print("Start training...")
-    timer()
-    emb = model.train(data=walks, num_step=1000000, log_step=2000, save_step=2)
-    timer(False)
-    np.save("cora.emb.npy", emb)
-
-def test4():
-    network = GraphContainer(find_meta(dataset_name), dloc)
-    print("Generating gt graph...")
-    timer()
-    gt = network.get_gt_graph()
-    timer(False)
-    print("Creating Skipgram model...")
-    timer()
-    model = Skipgram(window_size=5, num_skip=2, num_nsamp=15)
-    model.device = '/gpu:0'
-    model.build(num_vertices=gt.num_vertices(), learning_rate=0.001, opt=ADAM, batch_size=10000)
     timer(False)
     print("Generating random walk...")
     timer()
