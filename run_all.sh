d5122cac6519556912ba57be627f778e1a5a3d24
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-06-16 19:09:51 +0900

    add all hyper params

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 source activate network
-python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 1000000 -b 4096 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.001 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 10000 -nw 10 -b 4096 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
