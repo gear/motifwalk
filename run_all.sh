ee3aecbc88bd3e7ca4eaea129adbf694a411ac54
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-06-16 19:34:24 +0900

    exp cora emb dim, number of steps, and learning rate

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/bash
 
 source activate network
-python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 10000 -nw 10 -b 4096 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.0001 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.001 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.1 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 8 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 16 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 32 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 128 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 256 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 512 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 10000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 20000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 40000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 100000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 1000000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 2000000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
+python motifwalk.py -d cora -w undirected -l 80 -k 64 -t 20000000 -nw 10 -b 1024 -m skipgram -ws 5 -nn 15 -ns 2 -lr 0.01 --log_step 4000 --save_step 10000 --device="/cpu:0" --save_loc ./emb/
