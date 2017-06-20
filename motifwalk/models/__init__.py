b51b430f847f058302c371822104024cbac5f297
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-06-20 10:31:14 +0900

    abstract class for classifier

--- motifwalk/models/__init__.py
+++ motifwalk/models/__init__.py
@@ -13,3 +13,17 @@ class EmbeddingModel(metaclass=ABCMeta):
     @abstractmethod
     def train(self):
         pass
+
+class Classifier(metaclass=ABCMeta):
+
+    @abstractmethod
+    def build(self):
+        pass
+
+    @abstractmethod
+    def train(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
