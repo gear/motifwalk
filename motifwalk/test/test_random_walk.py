df94b2ec6643620b43a3882e2b950141a24448b0
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-06-24 12:57:25 +0900

    simple debug for motifwalk

--- motifwalk/test/test_random_walk.py
+++ motifwalk/test/test_random_walk.py
@@ -1,6 +1,8 @@
 from motifwalk.utils import find_meta, set_dataloc, get_metadata
 from motifwalk.utils.Graph import GraphContainer
 from motifwalk.walks import undirected_randomwalk
+from motifwalk.motifs.analysis import construct_motif_graph, filter_isolated
+from motifwalk.motifs import triangle
 import os
 from sys import argv
 
@@ -11,14 +13,26 @@ def test(test_number):
     if '1' == test_number:
         test1()
     elif '2' == test_number:
-        test2()
+        test2(triangle)
     else:
         print("Test have not yet defined")
 
 def test1():
+    """Simple random walk"""
     pack = GraphContainer(find_meta(dataset_name), dloc)
     gt = pack.get_gt_graph()
     walks, index = undirected_randomwalk(gt)
+    print(walks.shape)
+    print(index)
+    print(walks[:10])
+    print(walks[-10:])
+
+def test2(m_container):
+    """Random walk on motif graph created by triangle"""
+    pack = GraphContainer(find_meta(dataset_name), dloc)
+    motif_graph = construct_motif_graph(pack, m_container)
+    motif_view = filter_isolated(motif_graph)  # GraphView
+    walks, index = undirected_randomwalk(motif_view, 10, 1)
     print(walks.shape)
     print(index)
     print(walks[:10])
