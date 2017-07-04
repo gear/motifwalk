dee41ecbc79683ce04b987f54a438349b01c1aae
Hoang NT <hoangnt@net.c.titech.ac.jp>
2017-07-04 14:38:18 +0900

    tsubame scripts

--- motifwalk/motifs/tsubame_large_nets.sh
+++ motifwalk/motifs/tsubame_large_nets.sh
@@ -5,5 +5,7 @@
 cd /home/usr8/15M54097/motifwalk/motifwalk/motifs
 source activate network
 
-python z_score.py -d cora -f cora_zscore_ -k 4 -n 0 --dloc ./../../data
+python z_score.py -d amazon -f amazon_zscore_ -k 4 -n 10 --dloc ./../../data
+python z_score.py -d youtube -f youtube_zscore_ -k 4 -n 10 --dloc ./../../data
+python z_score.py -d blogcatalog -f blogcatalog_zscore_ -k 4 -n 10 --dloc ./../../data
 
