f04b0a60d54f29d892f2d08bda2049a61b1ae20f
Hoang NT <hoangnt@net.c.titech.ac.jp>
2017-07-04 19:39:24 +0900

    delete old tsubame file

--- motifwalk/motifs/tsubame_submit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-cd /home/usr8/15M54097/motifwalk/motifwalk/motifs
-t2sub -q V -W group_list=t2g-crest-deep-mu -l select=16:mpiprocs=16:mem=20gb -l place=scatter ./tsubame_large_nets.sh
